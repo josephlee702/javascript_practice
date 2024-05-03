43c273860c7a11be554025d5731b23240d3c16d2
Joseph Lee <jhjlee702@gmail.com>
2024-05-03 13:28:39 -0600

    create getHumanChoice function

--- rockpaperscissors/rockpaperscissors.js
+++ rockpaperscissors/rockpaperscissors.js
@@ -1,6 +1,8 @@
+getComputerChoice();
+getHumanChoice();
+
 function getComputerChoice(){
   let choice = Math.random();
-
   if (choice <= 0.33) {
     console.log("rock");
   }
@@ -13,4 +15,20 @@ function getComputerChoice(){
   console.log(choice);
 }
 
-getComputerChoice();
+function getHumanChoice() {
+  let userInput = prompt("Will you choose rock, paper, or scissors?");
+  // console.log(userInput);
+
+  if (userInput == "rock") {
+    console.log("You have chosen rock.");
+  }
+  else if (userInput == "paper") {
+    console.log("You have chosen paper.");
+  }
+  else if (userInput == "scissors") {
+    console.log("You have chosen scissors.");
+  }
+  else {
+    console.log("Please choose one of the given options.");
+  }
+}
