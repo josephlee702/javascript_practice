8b7e3667d634dd1e20f8c519d9439dc12de89ca8
Joseph Lee <jhjlee702@gmail.com>
2024-05-03 13:20:03 -0600

    fixed bug where it would not choose paper

--- rockpaperscissors/rockpaperscissors.js
+++ rockpaperscissors/rockpaperscissors.js
@@ -2,13 +2,13 @@ function getComputerChoice(){
   let choice = Math.random();
 
   if (choice <= 0.33) {
-    console.log("rock")
+    console.log("rock");
   }
-  else if (0.33 < choice <= 0.67) {
-    console.log("paper")
+  else if (choice <= 0.67) {
+    console.log("paper");
   }
   else {
-    console.log("scissors")
+    console.log("scissors");
   }
   console.log(choice);
 }
